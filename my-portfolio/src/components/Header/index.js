ffb8a3417df36e6dcd3d0f0a3f50e64d36a34518
truuue <noahvernhet@gmail.com>
2024-06-14 19:27:17 +0200

    [HEADER COMPONENT] Creating the base of the navbar adding some tailwind style and link

--- my-portfolio/src/components/Header/index.js
+++ my-portfolio/src/components/Header/index.js
@@ -2,8 +2,19 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <header className="">
-      <h1>My Portfolio</h1>
+    <header className="w-full flex justify-center items-center z-10 sticky mb-[1.75rem] top-[0.75rem]">
+      <div className=" w-4/5 flex justify-center backdrop-blur-md shadow border-solid rounded-xl">
+        <nav>
+          <ul className="flex flex-row gap-5 m-5">
+            <li className="hover:text-gray-500 transition-colors duration-300">
+              <a href="https://github.com/truuue">Github</a>
+            </li>
+            <li className="hover:text-gray-500 transition-colors duration-300">
+              <a href="https://www.linkedin.com/feed/">LinkedIn</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
