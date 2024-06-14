5a919a2a4133ef27dcfbc869496802f1798dc2c6
truuue <noahvernhet@gmail.com>
2024-06-15 00:20:03 +0200

    [JS] Change the name file

---
+++ my-portfolio/src/components/Main/Main.js
@@ -0,0 +1,91 @@
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <main className="relative w-full h-screen overflow-hidden">
+      {/* Just for testing some scrolling animation */}
+      <h2>Welcome to my portfolio</h2>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+      <p>Check out my projects and get in touch!</p>
+      <br></br>
+    </main>
+  );
+};
+
+export default Main;
