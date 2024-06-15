e82e185dbb971a532822b9247c7d3fbf10b80416
truuue <noahvernhet@gmail.com>
2024-06-15 23:20:18 +0200

    [HTML] First steps on the main component

--- my-portfolio/src/components/Main/Main.js
+++ my-portfolio/src/components/Main/Main.js
@@ -3,8 +3,14 @@ import "./Main.css";
 
 const Main = () => {
   return (
-    <main>
-      <Scroll />
+    <main className="h-screen">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-6xl font-bold text-center">
+            <span>&#128075;</span> Hello, I'm Noah
+          </h1>
+          <p className="text-lg text-center">I'm a Full Stack Developer</p>
+        </div>
+      {/** <Scroll /> */}
     </main>
   );
 };
