52dce6bb7d461d8ef93881a2f16aec4748e96fa2
truuue <noahvernhet@gmail.com>
2024-06-16 19:48:23 +0200

    [HTML / TAILWIND] Make the second step of my project with updated the "first view"

--- my-portfolio/src/components/Main/Main.js
+++ my-portfolio/src/components/Main/Main.js
@@ -1,16 +1,44 @@
 import Scroll from "../infiniteScrollingIcons/Scroll";
+import SlidingText from "../slidingText/slidingText";
 import "./Main.css";
 
 const Main = () => {
   return (
-    <main className="h-screen">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-6xl font-bold text-center">
-            <span>&#128075;</span> Hello, I'm Noah
-          </h1>
-          <p className="text-lg text-center">I'm a Full Stack Developer</p>
+    <main>
+      {/** Home section */}
+      <div
+        id="home"
+        className="h-screen flex flex-col items-center justify-center  mt-[-2rem]"
+      >
+        <h1 className="text-6xl font-bold text-center mb-5">
+          <span>&#128075;</span> Hello, I'm Noah
+        </h1>
+
+        {/** sliding text component */}
+        <SlidingText />
+      </div>
+
+      {/** About section */}
+      <div id="about" className="h-screen flex flex-col justify-center">
+        <h2 className="text-5xl font-bold text-center mb-10 underline">
+          About
+        </h2>
+
+        <div>
+          <p className="text-lg text-center">
+            Nice to meet you! like I said before my name is Noah and i'm a
+            fullstack developer (but I have a litle preferencis for the
+            front-end development <span>&#129312;</span>)
+          </p>
         </div>
-      {/** <Scroll /> */}
+
+        <h3 className="text-4xl font-bold text-center">My stacks</h3>
+
+        {/** infinite scroll component */}
+        <div className="flex justify-end">
+          <Scroll />
+        </div>
+      </div>
     </main>
   );
 };
