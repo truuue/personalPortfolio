acf01e7df7f8fd1ad37b8e09dd2d00216d1f4d05
truuue <noahvernhet@gmail.com>
2024-06-16 19:47:25 +0200

    [COMMENT] Adding some comment on the Header component

--- my-portfolio/src/components/Header/Header.js
+++ my-portfolio/src/components/Header/Header.js
@@ -2,20 +2,24 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <header className="w-full flex justify-center items-center z-10 sticky mb-[5rem] top-[0.75rem]">
+    <header className="w-full flex justify-center items-center z-10 sticky top-[0.75rem]">
       <div className="w-3/5 p-3 flex justify-center backdrop-blur-md shadow border-solid rounded-[2rem]">
         <nav className="w-4/5">
           <ul className="flex  justify-between font-medium">
             <li className="transition ease-in-out delay-50 hover:text-gray-600">
+              {/* Home */}
               <a href="#home">Home</a>
             </li>
             <li className="transition ease-in-out delay-50 hover:text-gray-600">
+              {/* About */}
               <a href="#about">About</a>
             </li>
             <li className="transition ease-in-out delay-50 hover:text-gray-600">
+              {/* Projects */}
               <a href="#projects">Projects</a>
             </li>
             <li className="transition ease-in-out delay-50 hover:text-gray-600">
+              {/* Contact */}
               <a href="#contact">Contact</a>
             </li>
           </ul>
@@ -26,16 +30,3 @@ const Header = () => {
 };
 
 export default Header;
-
-{
-  /**
-   * <ul className="flex flex-row gap-5 m-5">
-   * <li className="hover:text-gray-500 transition-colors duration-300">
-   * <a href="https://github.com/truuue">Github</a>
-   * </li>
-   * <li className="hover:text-gray-500 transition-colors duration-300">
-   * <a href="https://www.linkedin.com/feed/">LinkedIn</a>
-   * </li>
-   * </ul>
-   */
-}
