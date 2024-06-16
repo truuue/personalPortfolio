66cfaf488cef2546cb97f4a0f6e1407163f26b09
truuue <noahvernhet@gmail.com>
2024-06-16 23:12:02 +0200

    [REACT HOOK / JS] Adding a right click disabling feature for all the app

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import DisableRightClick from "./components/disableRightClick/disableRightClick";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <DisableRightClick>
+      <App />
+    </DisableRightClick>
   </React.StrictMode>
 );
 
