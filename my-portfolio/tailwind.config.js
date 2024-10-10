2f85e148b015aff4e33d5624d2748257ae17dd50
truuue <noahvernhet@gmail.com>
2024-10-10 16:05:14 +0200

    Add some extend feature, variants and plugins

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx,html,json}",
+    "./public/**/*.{js,ts,jsx,tsx,mdx,html,json}",
+  ],
   theme: {
     extend: {
       animation: {
         "loop-scroll": "loop-scroll 50s linear infinite",
         slide: "slide 6s ease-in-out infinite",
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+        reverse: "reverse var(--duration) linear infinite",
       },
       keyframes: {
         "loop-scroll": {
@@ -17,8 +23,38 @@ module.exports = {
           "40%, 60%": { transform: "translateY(0%)" },
           "67%, 100%": { transform: "translateY(-100%)" },
         },
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        "marquee-vertical": {
+          "0%": { transform: "translateY(0%)" },
+          "100%": { transform: "translateY(-100%)" },
+        },
       },
     },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      animation: ["group-hover"],
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".pause-animation": {
+          "animation-play-state": "paused",
+        },
+      };
+      addUtilities(newUtilities, ["group-hover"]);
+    },
+  ],
 };
