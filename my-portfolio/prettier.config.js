43af0179a4fedc4b53e208c299edeba6a17604b9
truuue <noahvernhet@gmail.com>
2024-10-10 16:04:22 +0200

    Add auto indentation on save with prettier

---
+++ my-portfolio/prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.js",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
